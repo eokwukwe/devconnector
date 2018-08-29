a6cfe02b9707b662d2c81188627c8404e8527d96
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-08-29 04:31:57 +0100

    completed register validation

--- validation/register.js
+++ validation/register.js
@@ -1,11 +1,45 @@
-const validator = require('validator'),
+const Validator = require('validator'),
   isEmpty = require('./is-empty');
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  if (!validator.isLength(data.name, { min: 2, max: 30 })) {
+  // The argument for isEmpty from Validator must be a string to work, so we need to convert the value from an empty field to an empty string by using our custom isEmpty function
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm Password field is required';
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
   }
 
   return {
