daefc3e58bcb653f2db5c184ab33962796b4099c
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-08-29 01:08:32 +0100

    added user register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,47 @@
 const express = require('express'),
+  User = require('../../models/User'),
+  gravatar = require('gravatar'),
+  bcrypt = require('bcryptjs'),
   router = express.Router();
 
 // @route  GET api/users/test
 // @desc   Tests users route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+// @route  GET api/users/register
+// @desc   Register user
+// @access Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
