09d902a9fa21d9eebe55ee491bfd7c0e2d4c6a53
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-08-30 00:31:45 +0100

    Create Post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,40 @@
 const express = require('express'),
+  mongoose = require('mongoose'),
+  passport = require('passport'),
   router = express.Router();
+
+const Post = require('../../models/Post'),
+  User = require('../../models/User'),
+  validatePostInput = require('../../validation/post');
 
 // @route  GET api/posts/test
 // @desc   Tests post route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+// @route  POST api/posts
+// @desc   Create post
+// @access Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check for validation errors
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then((post) => res.json(post));
+  }
+);
 
 module.exports = router;
