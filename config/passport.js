666583abdae9fd5141a29a27c7a2e09c218eb875
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-08-29 02:45:54 +0100

    added passport.js for authentication

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+// Authenntication for portected routes
+
+const JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt,
+  mongoose = require('mongoose'),
+  User = mongoose.model('users'), // same as User = require(../models/User)
+  jwtSecret = require('../config/keys');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtSecret.jwt_secret;
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+
+          return done(null, false);
+        })
+        .catch((err) => console.log(err));
+    })
+  );
+};
