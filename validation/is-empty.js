da2383f53cadb63d06b636501425ea00884751ed
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-08-29 03:32:20 +0100

    added validations for login and register

---
+++ validation/is-empty.js
@@ -0,0 +1,9 @@
+// Specialized isEmpty function for input validation
+
+const isEmpty = (value) =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
