57476d877bf5638e5ceda2fc93e0bcae71d1848f
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-08-30 02:41:02 +0100

    get all posts and delete post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,12 +5,36 @@ const express = require('express'),
 
 const Post = require('../../models/Post'),
   User = require('../../models/User'),
+  Profile = require('../../models/Profile'),
   validatePostInput = require('../../validation/post');
 
 // @route  GET api/posts/test
 // @desc   Tests post route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+// @route  GET api/posts
+// @desc   Get posts
+// @access Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) =>
+      res.status(404).json({ nopostfound: 'No posts available at the moment' })
+    );
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by ID
+// @access Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) =>
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
+    );
+});
 
 // @route  POST api/posts
 // @desc   Create post
@@ -34,6 +58,32 @@ router.post(
     });
 
     newPost.save().then((post) => res.json(post));
+  }
+);
+
+// @route  DELETE api/posts/:id
+// @desc   Delete post
+// @access Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: 'User not authorized' });
+          }
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnofound: 'No post found with that ID' })
+        );
+    });
   }
 );
 
