863895dabc2c1d059da9973aa5bda9438a71f377
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-09-19 00:03:39 +0100

    Navbar Footer Landing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App component</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
