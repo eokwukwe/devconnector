533f43d3ff736adeffa68d4d5a50b6256f33a985
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-09-19 03:13:36 +0100

    initial redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Landing from './components/Layout/Landing';
@@ -11,17 +13,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" component={Landing} exact />
-          <div className="container">
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path="/" component={Landing} exact />
+            <div className="container">
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import classnames from 'classnames';
-import { error } from 'util';
 
 class Register extends Component {
   state = {
