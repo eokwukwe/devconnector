47a5f789aaee9e0700342634ac3b6eda844bcf83
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-09-19 01:28:05 +0100

    Signin and Register added

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    error: {}
+  };
+
+  onChange = name => e => {
+    this.setState({
+      [name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Login</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={this.onChange('email')}
+                    autoFocus
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={this.onChange('password')}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  };
+
+  onChange = name => e => {
+    this.setState({
+      [name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    e.target.reset();
+  };
+
+  render() {
+    const { name, email, password, password2 } = this.state;
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-10 col-md-8 col-lg-6 m-auto">
+              <h1 className="display-4 text-center">Register</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    value={name}
+                    onChange={this.onChange('name')}
+                    autoFocus
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={this.onChange('email')}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={this.onChange('password')}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={password2}
+                    onChange={this.onChange('password2')}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
