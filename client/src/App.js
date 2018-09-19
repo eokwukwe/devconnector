26fb694ad9f5c1922dec1e8449f42106098e1048
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-09-19 01:28:31 +0100

    edit the layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Landing from './components/Layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" component={Landing} exact />
+          <div className="container">
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
-          <a className="navbar-brand" href="landing.html">
+          <NavLink className="navbar-brand" to="/">
             DevConnector
-          </a>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -19,23 +20,23 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="profiles.html">
+                <NavLink className="nav-link" to="/profiles">
                   {' '}
                   Developers
-                </a>
+                </NavLink>
               </li>
             </ul>
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="register.html">
-                  Sign Up
-                </a>
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <NavLink className="nav-link" to="/login">
                   Login
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
