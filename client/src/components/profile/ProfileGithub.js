b86fef9367395d96118f8c7f78c3f6a475d51eed
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-10-10 17:22:21 +0100

    ProfileByHandle action & sub components added

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileGithub extends Component {
+  render() {
+    return (
+      <div>
+        <h2>TODO: Profile Github</h2>
+      </div>
+    );
+  }
+}
+
+export default ProfileGithub;
---
+++ client/src/components/profile/ProfileHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h2>TODO: Profile Header</h2>
+      </div>
+    );
+  }
+}
+
+export default ProfileHeader;
