51c6fde7bd46aad9b8ff026c3469db89fc336af5
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-10-11 11:06:32 +0100

    ProfileAbout & ProfileCreds added

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
+import isEmpty from '../../utils/is-empty';
+
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+    const firstName = profile.user.name.trim().split(' ')[0];
+    const skills = profile.skills.map((skill, i) => (
+      <div key={i} className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
+
     return (
-      <div>
-        <h2>TODO: Profile About</h2>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">
+              {firstName}
+              's Bio
+            </h3>
+            {!isEmpty(profile.bio) ? (
+              <p className="lead">{profile.bio}</p>
+            ) : (
+              <p>{firstName} has no bio yet</p>
+            )}
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,10 +1,87 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+
+    const expItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {!exp.to ? (
+            ' Now'
+          ) : (
+            <Moment format="YYYY/MM/DD" className="ml-1">
+              {exp.to}
+            </Moment>
+          )}
+        </p>
+        <p>
+          <strong>Position: </strong>
+          {exp.title}
+        </p>
+        <p>
+          {exp.location && (
+            <span>
+              <strong>Location: </strong>
+              {exp.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description && (
+            <span>
+              <strong>Description: </strong>
+              {exp.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
+    const eduItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {!edu.to ? (
+            ' Now'
+          ) : (
+            <Moment format="YYYY/MM/DD" className="ml-1">
+              {edu.to}
+            </Moment>
+          )}
+        </p>
+        <p>
+          <strong>Degree: </strong>
+          {edu.degree}
+        </p>
+        <p>
+          <strong>Field of Study: </strong> {edu.fieldofstudy}
+        </p>
+      </li>
+    ));
+
     return (
-      <div>
-        <h2>TODO: Profile Creds</h2>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{expItems}</ul>
+          ) : (
+            <p className="text-center">No Experience listed</p>
+          )}
+        </div>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {eduItems.length > 0 ? (
+            <ul className="list-group">{eduItems}</ul>
+          ) : (
+            <p className="text-center">No Education listed</p>
+          )}
+        </div>
       </div>
     );
   }
