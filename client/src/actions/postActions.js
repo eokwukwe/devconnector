f9913eba009591fcf92215f4cca768ae3e258d2d
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-01-03 11:23:43 +0100

    comment display and delete

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,11 +6,13 @@ import {
   GET_POST,
   POST_LOADING,
   DELETE_POST,
-  GET_POSTS
+  GET_POSTS,
+  CLEAR_ERRORS
 } from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -111,8 +113,27 @@ export const removeLike = id => dispatch => {
 
 // Add comment
 export const addComment = (postId, comment) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, comment)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+//  Delete comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
@@ -131,5 +152,12 @@ export const addComment = (postId, comment) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   };
 };
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import Spinner from "../common/Spinner";
 import PostItem from "../posts/PostItem";
 import { getPost } from "../../actions/postActions";
 import CommentForm from "./CommentForm";
+import CommentFeed from "./CommentFeed";
 
 class Post extends Component {
   componentDidMount() {
@@ -21,6 +22,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed comments={post.comments} postId={post._id} />
         </div>
       );
     }
